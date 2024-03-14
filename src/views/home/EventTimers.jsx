35b0657f10b22f58900b615d131cf5477ace85d1
Kit Allen <kitallen23@gmail.com>
2024-03-15 03:58:49 +1100

    Feat: added button to reset timeline, moved logic around to accomodate this change

--- src/views/home/EventTimers.jsx
+++ src/views/home/EventTimers.jsx
@@ -1,38 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import {
-    getHours,
-    isEqual,
-    setHours,
-    setMilliseconds,
-    setMinutes,
-    setSeconds,
-} from "date-fns";
+import { useEffect, useRef } from "react";
 import styles from "@/styles/modules/event-timer.module.scss";
 
 import EventRegion, { CurrentTimeIndicator, TimeRow } from "./EventComponents";
 import META_EVENTS from "@/utils/meta_events";
-import { useTimer } from "@/utils/hooks/useTimer";
 import EventTimerContext from "./EventTimerContext";
 import { useResizeDetector } from "react-resize-detector";
 
-// Obtains the start time of a "time block"; a 2-hour period of time, relative to the
-// local timezone, that started on the last 1-hour time window.
-// A time block may be 1pm - 3pm, or 2am - 4am.
-const getCurrentTimeBlockStart = (offset = 0) => {
-    const now = new Date();
-    const hoursSinceMidnight = getHours(now);
-    const timeBlockStart = setMilliseconds(
-        setSeconds(
-            setMinutes(setHours(now, hoursSinceMidnight + offset), 0),
-            0
-        ),
-        0
-    );
-
-    return timeBlockStart;
-};
-
-const EventTimers = ({ offset }) => {
+const EventTimers = ({ currentTimeBlockStart }) => {
     const scrollParentRef = useRef(null);
 
     // This is the width of the entire timer component.
@@ -40,23 +14,6 @@ const EventTimers = ({ offset }) => {
     // value is always correct (as this ruler never gains any content)
     const { ref: widthRulerRef } = useResizeDetector();
     const width = widthRulerRef?.current?.scrollWidth || 0;
-
-    // Stores the start moment of the selected 2-hour time window
-    // (defaults to the current one)
-    // Note that this is relative to the local time, e.g. the first window of
-    // today is always 12:00am local time
-    const { key } = useTimer();
-    const [currentTimeBlockStart, setCurrentTimeBlockStart] = useState(
-        getCurrentTimeBlockStart()
-    );
-
-    // Update the current time block, but only when necessary
-    useEffect(() => {
-        const newCurrentTimeBlockStart = getCurrentTimeBlockStart(offset);
-        if (!isEqual(currentTimeBlockStart, newCurrentTimeBlockStart)) {
-            setCurrentTimeBlockStart(newCurrentTimeBlockStart);
-        }
-    }, [key, currentTimeBlockStart, offset]);
 
     useEffect(() => {
         if (scrollParentRef?.current) {
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,16 +1,76 @@
+import { useEffect, useMemo, useState } from "react";
 import globalStyles from "@/styles/modules/global-styles.module.scss";
 import styles from "@/styles/modules/home.module.scss";
 import {
     ChevronLeftSharp,
     ChevronRightSharp,
+    HistorySharp,
     HourglassTopSharp,
 } from "@mui/icons-material";
-import EventTimers from "./EventTimers";
-import { useState } from "react";
+import {
+    addMinutes,
+    getHours,
+    isEqual,
+    setHours,
+    setMilliseconds,
+    setMinutes,
+    setSeconds,
+} from "date-fns";
 import { Button } from "@mui/material";
 
+import EventTimers from "./EventTimers";
+import { useTimer } from "@/utils/hooks/useTimer";
+import { TIME_BLOCK_MINS } from "./EventComponents";
+import { useTheme } from "@/utils/theme-provider";
+
+// Obtains the start time of a "time block"; a 2-hour period of time, relative to the
+// local timezone, that started on the last 1-hour time window.
+// A time block may be 1pm - 3pm, or 2am - 4am.
+const getCurrentTimeBlockStart = (offset = 0) => {
+    const now = new Date();
+    const hoursSinceMidnight = getHours(now);
+    const timeBlockStart = setMilliseconds(
+        setSeconds(
+            setMinutes(setHours(now, hoursSinceMidnight + offset), 0),
+            0
+        ),
+        0
+    );
+
+    return timeBlockStart;
+};
+
 const Home = () => {
+    const { key, now } = useTimer();
+    const { colors } = useTheme();
     const [offset, setOffset] = useState(0);
+
+    // Stores the start moment of the selected 2-hour time window
+    // (defaults to the current one)
+    // Note that this is relative to the local time, e.g. the first window of
+    // today is always 12:00am local time
+    const [currentTimeBlockStart, setCurrentTimeBlockStart] = useState(
+        getCurrentTimeBlockStart()
+    );
+
+    // Update the current time block, but only when necessary
+    useEffect(() => {
+        const newCurrentTimeBlockStart = getCurrentTimeBlockStart(offset);
+        if (!isEqual(currentTimeBlockStart, newCurrentTimeBlockStart)) {
+            setCurrentTimeBlockStart(newCurrentTimeBlockStart);
+        }
+    }, [key, currentTimeBlockStart, offset]);
+
+    const timeBlockContainsNow = useMemo(() => {
+        const currentTimeBlockEnd = addMinutes(
+            currentTimeBlockStart,
+            TIME_BLOCK_MINS
+        );
+        if (now >= currentTimeBlockStart && now <= currentTimeBlockEnd) {
+            return true;
+        }
+        return false;
+    }, [currentTimeBlockStart, now]);
 
     return (
         <div className={styles.pageWrapper}>
@@ -34,6 +94,20 @@ const Home = () => {
                             </Button>
                             <Button
                                 variant="text"
+                                sx={{
+                                    minWidth: 0,
+                                    ":disabled": {
+                                        color: `${colors.muted}80`,
+                                    },
+                                }}
+                                color="muted"
+                                onClick={() => setOffset(0)}
+                                disabled={timeBlockContainsNow}
+                            >
+                                <HistorySharp />
+                            </Button>
+                            <Button
+                                variant="text"
                                 sx={{ minWidth: 0 }}
                                 color="muted"
                                 onClick={() => setOffset(offset + 1)}
@@ -43,7 +117,7 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <EventTimers offset={offset} />
+                <EventTimers currentTimeBlockStart={currentTimeBlockStart} />
             </div>
         </div>
     );
