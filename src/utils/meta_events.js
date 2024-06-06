b2419510fd5250ce01de11eddf7fb6002227c932
Kit Allen <kitallen23@gmail.com>
2024-06-06 22:04:37 +1000

    Feat: finished adding configs for PoF zones, added configs for LWS4 zones, refactored config for timers to use enums for colors, added event "on complete" config

--- src/utils/meta_events.js
+++ src/utils/meta_events.js
@@ -1,14 +1,30 @@
+const COLORS = {
+    orange: "orange",
+    yellow: "yellow",
+    green: "green",
+    aqua: "aqua",
+    blue: "blue",
+    purple: "purple",
+    pink: "pink",
+    gray: "gray",
+};
+const ON_COMPLETE_TYPES = {
+    completeEvent: "complete_event",
+    completeArea: "complete_area",
+    none: "none",
+};
 const META_EVENTS = [
     {
         key: "core_tyria",
         name: "CORE",
-        color: "gray",
+        color: COLORS.gray,
         sub_areas: [
             {
                 key: "world_bosses",
                 name: "World Bosses",
-                color: "gray",
+                color: COLORS.gray,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeEvent,
                 phases: [
                     {
                         key: "admiral_taidha_covington",
@@ -105,8 +121,9 @@ const META_EVENTS = [
             {
                 key: "hard_bosses",
                 name: "Hard Bosses",
-                color: "gray",
+                color: COLORS.gray,
                 type: "fixed_time",
+                onComplete: ON_COMPLETE_TYPES.completeEvent,
                 phases: [
                     {
                         key: "tequatl_the_sunless",
@@ -137,12 +154,13 @@ const META_EVENTS = [
             {
                 key: "ley_line_anomaly",
                 name: "Ley-Line Anomaly",
-                color: "gray",
+                color: COLORS.gray,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "ley_line_anomaly__iron_marches",
-                        name: "Iron Marches",
+                        name: "LLA: Iron Marches",
                         wikiUrl:
                             "https://wiki.guildwars2.com/wiki/Defeat_the_Ley-Line_Anomaly_to_disperse_its_destructive_energy_before_it_overloads",
                         start: 140,
@@ -151,7 +169,7 @@ const META_EVENTS = [
                     },
                     {
                         key: "ley_line_anomaly__gendarran_fields",
-                        name: "Gendarran Fields",
+                        name: "LLA: Gendarran Fields",
                         wikiUrl:
                             "https://wiki.guildwars2.com/wiki/Defeat_the_Ley-Line_Anomaly_to_disperse_its_destructive_energy_before_it_overloads",
                         start: 260,
@@ -160,7 +178,7 @@ const META_EVENTS = [
                     },
                     {
                         key: "ley_line_anomaly__timberline_falls",
-                        name: "Timberline Falls",
+                        name: "LLA: Timberline Falls",
                         wikiUrl:
                             "https://wiki.guildwars2.com/wiki/Defeat_the_Ley-Line_Anomaly_to_disperse_its_destructive_energy_before_it_overloads",
                         start: 20,
@@ -174,13 +192,14 @@ const META_EVENTS = [
     {
         key: "living_world_season_2",
         name: "LWS2",
-        color: "yellow",
+        color: COLORS.yellow,
         sub_areas: [
             {
                 key: "dry_top",
                 name: "Dry Top",
-                color: "yellow",
+                color: COLORS.yellow,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.none,
                 phases: [
                     {
                         key: "sandstorm",
@@ -207,13 +226,14 @@ const META_EVENTS = [
     {
         key: "heart_of_thorns",
         name: "HoT",
-        color: "green",
+        color: COLORS.green,
         sub_areas: [
             {
                 key: "verdant_brink",
                 name: "Verdant Brink",
-                color: "green",
+                color: COLORS.green,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "verdant_brink__night_and_the_enemy",
@@ -248,8 +268,9 @@ const META_EVENTS = [
             {
                 key: "auric_basin",
                 name: "Auric Basin",
-                color: "green",
+                color: COLORS.green,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "auric_basin__challenges",
@@ -287,8 +308,9 @@ const META_EVENTS = [
             {
                 key: "tangled_depths",
                 name: "Tangled Depths",
-                color: "green",
+                color: COLORS.green,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "tangled_depths__chak_gerent",
@@ -318,8 +340,9 @@ const META_EVENTS = [
             {
                 key: "dragons_stand",
                 name: "Dragon's Stand",
-                color: "green",
+                color: COLORS.green,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "dragons_stand__blighting_towers",
@@ -337,13 +360,14 @@ const META_EVENTS = [
     {
         key: "living_world_season_3",
         name: "LWS3",
-        color: "gray",
+        color: COLORS.gray,
         sub_areas: [
             {
                 key: "lake_doric",
                 name: "Lake Doric",
-                color: "gray",
+                color: COLORS.gray,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeEvent,
                 phases: [
                     {
                         key: "norans_homestead",
@@ -379,13 +403,14 @@ const META_EVENTS = [
     {
         key: "path_of_fire",
         name: "PoF",
-        color: "orange",
+        color: COLORS.orange,
         sub_areas: [
             {
                 key: "crystal_oasis",
                 name: "Crystal Oasis",
-                color: "orange",
+                color: COLORS.orange,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "crystal_oasis__collect_coins",
@@ -410,8 +435,9 @@ const META_EVENTS = [
             {
                 key: "desert_highlands",
                 name: "Desert Highlands",
-                color: "orange",
+                color: COLORS.orange,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "desert_highlands__buried_treasure",
@@ -427,8 +453,9 @@ const META_EVENTS = [
             {
                 key: "elon_riverlands",
                 name: "Elon Riverlands",
-                color: "orange",
+                color: COLORS.orange,
                 type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
                 phases: [
                     {
                         key: "elon_riverlands__path_to_ascension",
@@ -445,6 +472,139 @@ const META_EVENTS = [
                         wikiUrl:
                             "https://wiki.guildwars2.com/wiki/Shift_beyond_reality_to_confront_the_crazed_doppelganger",
                         start: 115,
+                        duration: 20,
+                        frequency: 120,
+                    },
+                ],
+            },
+            {
+                key: "the_desolation",
+                name: "The Desolation",
+                color: COLORS.orange,
+                type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
+                phases: [
+                    {
+                        key: "the_desolation__junundu_rising",
+                        name: "Junundu Rising",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Junundu_Rising",
+                        start: 30,
+                        duration: 20,
+                        frequency: 60,
+                    },
+                    {
+                        key: "the_desolation__maws_of_torment",
+                        name: "Maws of Torment",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Maws_of_Torment",
+                        start: 60,
+                        duration: 20,
+                        frequency: 120,
+                    },
+                ],
+            },
+            {
+                key: "domain_of_vabbi",
+                name: "Domain of Vabbi",
+                color: COLORS.orange,
+                type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
+                phases: [
+                    {
+                        key: "domain_of_vabbi__forged_with_fire",
+                        name: "Forged with Fire",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Forged_with_Fire",
+                        start: 60,
+                        duration: 30,
+                        frequency: 60,
+                    },
+                    {
+                        key: "domain_of_vabbi__serpents_ire",
+                        name: "Serpents' Ire",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Serpents'_Ire",
+                        start: 30,
+                        duration: 30,
+                        frequency: 120,
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        key: "living_world_season_4",
+        name: "LWS4",
+        color: COLORS.purple,
+        sub_areas: [
+            {
+                key: "domain_of_istan",
+                name: "Domain of Istan",
+                color: COLORS.purple,
+                type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
+                phases: [
+                    {
+                        key: "domain_of_istan__palawadan",
+                        name: "Palawadan",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Palawadan,_Jewel_of_Istan_(meta_event)",
+                        start: 105,
+                        duration: 30,
+                        frequency: 120,
+                    },
+                ],
+            },
+            {
+                key: "jahai_bluffs",
+                name: "Jahai Bluffs",
+                color: COLORS.purple,
+                type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
+                phases: [
+                    {
+                        key: "jahai_bluffs__escorts",
+                        name: "Escorts",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Dangerous_Prey",
+                        start: 60,
+                        duration: 15,
+                        frequency: 120,
+                    },
+                    {
+                        key: "jahai_bluffs__death_branded_shatterer",
+                        name: "Death Branded Shatterer",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Destroy_the_Death-Branded_Shatterer",
+                        start: 75,
+                        duration: 15,
+                        frequency: 120,
+                    },
+                ],
+            },
+            {
+                key: "thunderhead_peaks",
+                name: "Thunderhead Peaks",
+                color: COLORS.purple,
+                type: "periodic",
+                onComplete: ON_COMPLETE_TYPES.completeArea,
+                phases: [
+                    {
+                        key: "thunderhead_peaks__the_oil_floes",
+                        name: "The Oil Floes",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/The_Oil_Floes",
+                        start: 45,
+                        duration: 15,
+                        frequency: 120,
+                    },
+                    {
+                        key: "thunderhead_peaks__thunderhead_keep",
+                        name: "Thunderhead Keep",
+                        wikiUrl:
+                            "https://wiki.guildwars2.com/wiki/Thunderhead_Keep_(meta_event)",
+                        start: 105,
                         duration: 20,
                         frequency: 120,
                     },
