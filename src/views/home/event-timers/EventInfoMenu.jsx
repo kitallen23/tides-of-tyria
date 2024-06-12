343cde090243e5a45553a075153987800cdbd5a2
Kit Allen <kitallen23@gmail.com>
2024-06-12 11:39:35 +1000

    Feat: changed event menu to render primary color if event is gray (for better contrast), changed menu positioning to ensure the menu can't go off the right side of the event area

--- src/views/home/event-timers/EventInfoMenu.jsx
+++ src/views/home/event-timers/EventInfoMenu.jsx
@@ -5,8 +5,14 @@ import Portal from "@/components/Portal";
 import EventTimerContext from "./EventTimerContext";
 import classNames from "classnames";
 import { useTheme } from "@/utils/theme-provider";
-import { adjustLuminance } from "@/utils/color";
+import {
+    adjustLuminance,
+    ensureContrast,
+    isContrastEnough,
+} from "@/utils/color";
 import { ClearSharp } from "@mui/icons-material";
+
+const MENU_WIDTH = 200;
 
 const EventInfoMenu = () => {
     const {
@@ -18,6 +24,7 @@ const EventInfoMenu = () => {
 
     const [containerDimensions, setContainerDimensions] = useState(null);
 
+    // Find & store information we need to position the menu
     useEffect(() => {
         const pageContentElement = document.getElementById("page-content");
         const pageContentBoundingBox =
@@ -30,6 +37,7 @@ const EventInfoMenu = () => {
 
     const [anchor, setAnchor] = useState(null);
 
+    // Find the position to place the menu
     useEffect(() => {
         try {
             const selectedElement = eventWrapperRef.current.querySelector(
@@ -40,17 +48,30 @@ const EventInfoMenu = () => {
             }
             const boundingBox = selectedElement.getBoundingClientRect();
 
-            // TODO: Update this to detect width of final element & anchor it to
-            // right, if necessary
-            setAnchor({
-                top: `${
-                    boundingBox.bottom -
-                    containerDimensions.top +
-                    containerDimensions.scrollTop +
-                    4
-                }px`,
-                left: `${boundingBox.left}px`,
-            });
+            if (boundingBox.left + MENU_WIDTH > containerDimensions.right) {
+            // This means that the menu is "poking out" of the right side of the
+            // event area, so anchor it to the right of the bounding box
+                setAnchor({
+                    top: `${
+                        boundingBox.bottom -
+                        containerDimensions.top +
+                        containerDimensions.scrollTop +
+                        4
+                    }px`,
+                    left: `${boundingBox.right - MENU_WIDTH}px`,
+                });
+            } else {
+                // Menu can fit, so attach it left-aligned as usual
+                setAnchor({
+                    top: `${
+                        boundingBox.bottom -
+                        containerDimensions.top +
+                        containerDimensions.scrollTop +
+                        4
+                    }px`,
+                    left: `${boundingBox.left}px`,
+                });
+            }
         } catch {
             setAnchor(null);
         }
@@ -61,7 +82,8 @@ const EventInfoMenu = () => {
         eventContainerWidth,
     ]);
 
-    const { colors } = useTheme();
+    // Color calculations
+    const { colors, mode } = useTheme();
     const eventBackground = useMemo(
         () => colors?.[selectedEvent?.color] ?? undefined,
         [selectedEvent?.color, colors]
@@ -71,6 +93,36 @@ const EventInfoMenu = () => {
             eventBackground ? adjustLuminance(eventBackground, -40) : undefined,
         [eventBackground]
     );
+
+    const schemeColorString = useMemo(
+        () =>
+            selectedEvent?.color === "gray"
+                ? "primary"
+                : selectedEvent?.color || undefined,
+        [selectedEvent?.color]
+    );
+
+    const highlightColor = useMemo(() => {
+        const schemeColor = colors?.[schemeColorString] ?? undefined;
+        const background = colors.background;
+        if (schemeColor) {
+            const schemeColorHasEnoughContrast = isContrastEnough(
+                schemeColor,
+                colors.background
+            );
+            if (schemeColorHasEnoughContrast) {
+                return schemeColor;
+            } else {
+                return ensureContrast(
+                    schemeColor,
+                    background,
+                    mode === "light" ? "darken" : "lighten"
+                );
+            }
+        } else {
+            return undefined;
+        }
+    }, [schemeColorString, colors, mode]);
 
     if (!anchor || !selectedEvent) {
         return null;
@@ -95,7 +147,7 @@ const EventInfoMenu = () => {
                 </div>
                 <div
                     className={styles.eventTitle}
-                    style={{ color: eventBackground }}
+                    style={{ color: highlightColor }}
                 >
                     {selectedEvent.name}
                 </div>
