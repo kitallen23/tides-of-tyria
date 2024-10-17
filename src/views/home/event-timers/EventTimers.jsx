23cba788286516b8b3feee7f92368981a8358870
Kit Allen <kitallen23@gmail.com>
2024-10-17 12:13:03 +1100

    Fix: removed two console logs

--- src/views/home/event-timers/EventTimers.jsx
+++ src/views/home/event-timers/EventTimers.jsx
@@ -119,7 +119,6 @@ const EventTimers = () => {
             }
             let eventConfig = JSON.parse(eventConfigString);
             eventConfig = cleanEventConfig(eventConfig, dailyReset);
-            console.log(`eventConfig after cleaning: `, eventConfig);
 
             // Merge event config with new default event config if versions
             // don't match
@@ -128,7 +127,6 @@ const EventTimers = () => {
                 META_EVENTS?.[0]?.version
             ) {
                 eventConfig = resetConfigToDefault(eventConfig, META_EVENTS);
-                console.log(`eventConfig after resetting: `, eventConfig);
             }
 
             set_eventConfig(eventConfig);
