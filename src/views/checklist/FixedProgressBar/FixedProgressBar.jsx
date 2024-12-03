5562c97cc0b4c06fd387dbf96e2f40a9c8e43c5b
Kit Allen <kitallen23@gmail.com>
2024-12-03 14:27:19 +1100

    Feat: added daily & weekly progress bars

---
+++ src/views/checklist/FixedProgressBar/FixedProgressBar.jsx
@@ -0,0 +1,45 @@
+import { useMemo } from "react";
+import classNames from "classnames";
+
+import styles from "./fixed-progress-bar.module.scss";
+
+const FixedProgressBar = ({ checklistItems, position, color }) => {
+    const numCompleted = useMemo(
+        () => checklistItems?.filter(item => item.isComplete).length || 0,
+        [checklistItems]
+    );
+    const numTotal = useMemo(
+        () => checklistItems?.length || 0,
+        [checklistItems]
+    );
+    const percentageComplete = useMemo(
+        () => (numTotal ? (numCompleted / numTotal) * 100 : 0),
+        [numCompleted, numTotal]
+    );
+
+    return (
+        <>
+            <div
+                className={classNames(styles.progressBar, {
+                    [styles.left]: position === "left",
+                })}
+            >
+                <div
+                    className={styles.bar}
+                    style={{
+                        height: `${percentageComplete}%`,
+                        background: color,
+                    }}
+                >
+                    <div className={styles.progressText}>
+                        <div className={styles.text}>
+                            {numCompleted} / {numTotal}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default FixedProgressBar;
