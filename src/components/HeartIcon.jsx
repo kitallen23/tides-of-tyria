74e9e05dd03c62a4c2a3332aef9523c452567dbc
Kit Allen <kitallen23@gmail.com>
2024-12-04 18:25:39 +1100

    Feat: added attribution

---
+++ src/components/HeartIcon.jsx
@@ -0,0 +1,28 @@
+const HeartIcon = props => (
+    <svg
+        version="1.0"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 300.000000 300.000000"
+        preserveAspectRatio="xMidYMid meet"
+        width="1em"
+        height="1em"
+        {...props}
+    >
+        <g
+            transform="translate(0,300) scale(0.1,-0.1)"
+            fill="currentColor"
+            stroke="none"
+        >
+            <path
+                d="M560 2455 l0 -195 -195 0 -195 0 0 -385 0 -385 195 0 195 0 0 -195 0
+       -195 195 0 195 0 0 -195 0 -195 195 0 195 0 0 -195 0 -195 195 0 195 0 2 193
+       3 192 193 3 192 2 0 195 0 195 195 0 195 0 0 195 0 195 195 0 195 0 0 385 0
+       385 -192 2 -193 3 -3 193 -2 192 -390 0 -390 0 0 -190 0 -190 -195 0 -195 0 0
+       190 0 190 -390 0 -390 0 0 -195z m390 -380 l0 -195 -195 0 -195 0 0 189 0 190
+       33 4 c17 2 105 5 195 5 l162 2 0 -195z"
+            />
+        </g>
+    </svg>
+);
+
+export default HeartIcon;
